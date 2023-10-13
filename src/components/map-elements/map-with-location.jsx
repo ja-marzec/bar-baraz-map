aa0faf957c526e9471c442061410a9101f767e0d
ja-marzec <hubert.marzecki@gmail.com>
2023-10-13 13:36:45 +0200

    removed for phone

--- src/components/map-elements/map-with-location.jsx
+++ src/components/map-elements/map-with-location.jsx
@@ -12,7 +12,7 @@ export const MapWithLocation = ({ children, position }) => {
       maxZoom={18}
       scrollWheelZoom
       style={{ height: "96vh", maxWidth: "100vw" }}
-      zoomControl={false}
+      zoomControl={window.innerWidth > 500}
     >
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
